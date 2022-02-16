6ee192af4cb4b07cec35ab14abd7b6c58973acd1
Rodney chappell <tookie@Rodneys-Mini.attlocal.net>
2022-02-16 00:26:19 -0500

    commiting JS changes

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,38 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: 'input',
+    name: 'email',
+    message: `What's your email address?`,
+    validate: function (value) {
+        let pass = value.match(
+            /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+        );
+        if (pass) {
+            return true;
+        }
+        return 'Please enter a valid email address!';
+    },
+},
+{
+    type: 'input',
+    name: 'github',
+    message: `What's your GitHub User Name?`,
+
+},
+{
+    type: 'input',
+    name: 'title',
+    message: `What is the title of your project?`,
+},
+{
+    type: 'input',
+    name: 'description',
+    message: `Please write a description of your project`,
+
+},
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
