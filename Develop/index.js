ff5fdc5f88b193446b274c23640e80f6eec78cb9
Rodney chappell <tookie@Rodneys-Mini.attlocal.net>
2022-02-16 00:29:13 -0500

    commiting JS changes

--- Develop/index.js
+++ Develop/index.js
@@ -35,7 +35,11 @@ const questions = [{
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = (fileName, data) => {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.error(err) : console.log(success)
+    );
+}
 
 // TODO: Create a function to initialize app
 function init() {}
